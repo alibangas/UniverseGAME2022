f7d8e99262576e18834ed654bd1eb43ca2d0be78
alibangas <aaybangash@gmail.com>
2022-11-17 21:44:53 -0500

    Updated some changes
    Added some changes

---
+++ UniverseGameFall2022/Assets/Scripts/PlanetSelectionScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSelectionScript : MonoBehaviour
+{
+    //public Transform refObject;
+    //public void SelectPlanets ()
+    //{
+    //    refObject.GetComponent<zoomScript>().ButtonPress();
+
+    //}
+}
